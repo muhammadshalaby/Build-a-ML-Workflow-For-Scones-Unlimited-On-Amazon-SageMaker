035fc7da7d7c57ed95ebad2f93657b0fcbeba00e
muhammad_shalaby <muhammadshalaby@icloud.com>
2022-08-07 06:39:37 -0700

    Add files via upload

---
+++ lambda.py
@@ -0,0 +1,101 @@
+# serializeImageData
+
+import json
+import boto3
+import base64
+
+s3 = boto3.client('s3')
+
+def lambda_handler(event, context):
+    """A function to serialize target data from S3"""
+    
+    # Get the s3 address from the Step Function event input
+    key = event['s3_key']
+    bucket = event['s3_bucket']
+    
+    # Download the data from s3 to /tmp/image.png
+    boto3.resource('s3').Bucket(bucket).download_file(key, "/tmp/image.png")
+    
+    # We read the data from a file
+    with open("/tmp/image.png", "rb") as f:
+        image_data = base64.b64encode(f.read())
+
+    # Pass the data back to the Step Function
+    print("Event:", event.keys())
+    return {
+        'statusCode': 200,
+        'body': {
+            "image_data": image_data,
+            "s3_bucket": bucket,
+            "s3_key": key,
+            "inferences": []
+        }
+    }
+
+
+
+
+# classification
+
+import json
+import base64
+import boto3
+
+ENDPOINT = 'image-classification-2022-08-07-09-08-40-850'
+runtime = boto3.client('runtime.sagemaker')
+
+
+def lambda_handler(event, context):
+
+    # Decode the image data
+    image = base64.b64decode(event['body']['image_data'])
+
+    # Instantiate a Predictor
+    predictor = runtime.invoke_endpoint(EndpointName=ENDPOINT,
+                                        ContentType='image/png',
+                                        Body=image)
+
+    # For this model the IdentitySerializer needs to be "image/png"
+    # predictor.serializer=IdentitySerializer("image/png")
+
+    # Make a prediction:
+    inferences = predictor['Body'].read()
+
+    # We return the data back to the Step Function
+    event["inferences"] = inferences.decode('utf-8')
+    return {
+        'statusCode': 200,
+        'body': json.dumps(event)
+    }
+
+
+
+
+# Confidence Inferences
+
+import json
+
+
+THRESHOLD = .93
+
+def lambda_handler(event, context):
+
+    # Get the inferences from the event
+
+    body = json.loads(event['body'])
+    inferences = json.loads(body['inferences'])
+
+    # Check if any values in our inferences are above THRESHOLD
+    meets_threshold = any(x > THRESHOLD for x in inferences)
+
+    # If our threshold is met, pass our data back out of the
+    # Step Function, else, end the Step Function with an error
+    if meets_threshold:
+        pass
+    else:
+        raise ValueError("THRESHOLD_CONFIDENCE_NOT_MET")
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps(event)
+    }
